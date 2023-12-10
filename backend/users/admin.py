96e08b13a2a3e89b59afe084c360eae4f07ab87a
Andrew Kirov <AndrewKirov@me.com>
2023-12-10 14:32:50 +0500

    add dev branch and start my dimpoma

---
+++ backend/users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Админ-зона пользователей Django"""
+    form = CustomUserChangeForm
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'role', 'admin_status', 'is_active',)
+    search_fields = ('username', 'email', 'is_active',)
+    list_filter = ('username', 'email', 'is_active',)
+    list_editable = ('is_active',)
+    empty_value_display = '-пусто-'
+
+    def admin_status(self, obj):
+        return 'Админ' if obj.is_admin else 'Обычный юзер'
+    admin_status.short_description = 'Статус пользователя'
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        if obj:
+            fieldsets += (("Change Password", {"fields": ("password",)}),)
+        return fieldsets
---
+++ backend/users/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
