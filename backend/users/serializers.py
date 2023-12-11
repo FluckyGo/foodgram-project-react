c23782f4d3bad14e99e602a9ced568a39cbacccb
Andrew Kirov <AndrewKirov@me.com>
2023-12-11 19:02:18 +0500

    fix users ser-zer. add password hashing

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,10 +1,15 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.contrib.auth.hashers import make_password
 from .models import CustomUser
 
-USERNAME_REGEX = r'^[\w.@+-]+\z'
+USERNAME_REGEX = r'^[\w.@+-]+$'
+EMAIL_REGEX = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class CustomUserReadSerializer(serializers.ModelSerializer):
+    """ Сериализатор для чтения данных о пользователе из БД.  """
     is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
@@ -14,6 +19,50 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
-        if not user.is_anonymous:
-            return Follow.objects.filter(user=user, author=obj).exists()
+        # if not user.is_anonymous:
+        #    return Follow.objects.filter(user=user, author=obj).exists()
         return False
+
+
+class CustomUserWriteSerializer(serializers.ModelSerializer):
+    """ Сериализатор для записи данных о пользователе в БД.
+    Регистрация пользователя.  """
+    email = serializers.EmailField(
+        required=True,
+        validators=[RegexValidator(regex=EMAIL_REGEX,)]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[RegexValidator(regex=USERNAME_REGEX,)]
+    )
+    first_name = serializers.CharField(required=True,)
+    last_name = serializers.CharField(required=True,)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'email', 'username', 'first_name',
+                  'last_name', 'password')
+        extra_kwargs = {'password': {'write_only': True}, }
+
+    def create(self, validated_data):
+        email = validated_data['email']
+        username = validated_data['username']
+
+        existing_user_by_email = CustomUser.objects.filter(email=email).first()
+        existing_user_by_username = CustomUser.objects.filter(
+            username=username).first()
+
+        if existing_user_by_email:
+            raise ValidationError(
+                'Пользователь с таким адресом почты уже существует!',
+                code=status.HTTP_400_BAD_REQUEST)
+
+        if existing_user_by_username:
+            raise ValidationError(
+                'Пользователь с таким ником существует!',
+                code=status.HTTP_400_BAD_REQUEST)
+
+        user = CustomUser.objects.create(**validated_data)
+        user.password = make_password(validated_data['password'])
+        user.save()
+        return user
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,19 +1,42 @@
-from rest_framework import viewsets
-from rest_framework.permissions import (AllowAny)
+from rest_framework import viewsets, status
+from rest_framework.permissions import (
+    AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly)
 from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserReadSerializer, CustomUserWriteSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
-    serializer_class = CustomUserSerializer
 
-    @action(methods=['get'], detail=True)
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return CustomUserReadSerializer
+        return CustomUserWriteSerializer
+
+    @action(
+        methods=['get'],
+        detail=False,
+        permission_classes=(IsAuthenticated,)
+    )
     def me(self, request):
-        ...
+        ''' Получение профиля текущего пользователя. '''
+        instance = self.request.user
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'],
+            detail=False,
+            permission_classes=(IsAuthenticated,)
+            )
     def set_password(self, request):
-        ...
+        ''' Смена пароля текущего пользователя. '''
+        new_password = self.request.data.get('new_password')
+
+        request.user.password = make_password(new_password)
+        request.user.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
