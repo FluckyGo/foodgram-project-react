bec3d2b49a9235b3992be83ca093b1dfaf2c165b
Andrew Kirov <AndrewKirov@me.com>
2023-12-24 18:18:45 +0500

    fix settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 
-# DEBUG = os.getenv('DEBUG', '') == 'True'
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
@@ -33,8 +32,6 @@ INSTALLED_APPS = [
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
     'followers.apps.FollowersConfig',
-
-
 ]
 
 MIDDLEWARE = [
@@ -70,8 +67,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
